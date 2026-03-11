a0198813f84af89ff717c802c3e12f46ab5d1c39
ramjanss <ramjansocial@gmail.com>
2026-03-11 16:30:22 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const GP_CONFIG = {
+  name: "Gram Panchayat Wandhali",
+  village: "Wandhali",
+  taluka: "Warora",
+  district: "Chandrapur",
+  state: "Maharashtra",
+  pincode: "442906",
+  phone: "+91 9764444266",
+  email: "gpwandhali@gmail.com",
+  website: "https://wandhali.in"
+};
